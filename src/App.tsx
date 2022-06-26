ac8646fa851468cabd422ed1de22d2a20a855546
gcruzdev <gabrielasnts30@gmail.com>
2022-06-25 23:32:10 -0300

    routes and class player

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 
+import { ApolloProvider } from "@apollo/client"
 import { Event } from "./pages/Event"
+import { Router } from "./Router"
+import { client } from "./lib/apollo"
+import { BrowserRouter } from "react-router-dom"
 
 function App() {
   return (
-    <Event/>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
